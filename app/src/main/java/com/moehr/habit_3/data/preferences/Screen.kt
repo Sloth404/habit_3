c5e62489eb85af0fc59354b867ae6dcab7fcf6c5
Jōji <gpgerov+github@gmail.com>
2025-06-27 15:41:11 +0200

    #31: cleanup

--- app/src/main/java/com/moehr/habit_3/data/preferences/Screen.kt
+++
@@ -1,9 +0,0 @@
-package com.moehr.habit_3.data.preferences
-
-enum class Screen(val value : String) {
-    SETTINGS("settings"),
-    OVERVIEW("overview"),
-    STATISTICS("statistics"),
-    DETAIL("detail"),
-    EDIT_HABIT("habit")
-}
--- app/src/main/java/com/moehr/habit_3/data/preferences/SettingsSelections.kt
+++
@@ -1,6 +0,0 @@
-package com.moehr.habit_3.data.preferences
-
-enum class SettingsSelections(val id : String) {
-    PUSH("menu_push"),
-    THEME("menu_theme")
-}
