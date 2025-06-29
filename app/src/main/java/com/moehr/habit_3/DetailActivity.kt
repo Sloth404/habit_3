d374b564dbdd4a3e71e8055f6d6bfde366de969a
Jōji <gpgerov+github@gmail.com>
2025-06-30 01:00:47 +0200

    #58: nearly perfect consistency
    - headers of fragments in main activity look the same now
    - headers in detail- & editHabit-activity look the same now
    - still small differences between those two groups tho

--- app/src/main/java/com/moehr/habit_3/DetailActivity.kt
+++ app/src/main/java/com/moehr/habit_3/DetailActivity.kt
@@ -41,7 +41,7 @@ class DetailActivity : AppCompatActivity() {
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.detail)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
