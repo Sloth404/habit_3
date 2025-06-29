815aa45bbbd2bcb5e7f1a4e340d2bc62d6bd371b
Jōji <gpgerov+github@gmail.com>
2025-06-29 19:18:50 +0200

    #55: removed bottom inset - menu feeled too far up...

--- app/src/main/java/com/moehr/habit_3/MainActivity.kt
+++ app/src/main/java/com/moehr/habit_3/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
